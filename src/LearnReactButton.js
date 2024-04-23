ab8d45c3e210a25bae8a4cb92cb1bc5d3aabbfcf
Maxton Green <GreenMC@cougars.sf.edu>
2024-04-23 17:34:46 -0400

    Icon Gone

---
+++ src/LearnReactButton.js
@@ -0,0 +1,12 @@
+export default function LearnReactButton() {
+    return (
+        <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+        >
+            Learn React... or not
+        </a>
+    );
+}
