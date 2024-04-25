72dc499b6896c56a14d860045302777287391c95
Maxton Green <GreenMC@cougars.sf.edu>
2024-04-25 19:31:50 -0400

    removed  react app info button

--- src/App.js
+++ src/App.js
@@ -34,10 +34,6 @@ function App() {
       <div className="App">
         <header className="App-header">
           <Navbar /> {/* Include the navigation bar */}
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <LearnReactButton />
         </header>
         <Routes>
           <Route path="/cart" exact element={<Cart />} />
