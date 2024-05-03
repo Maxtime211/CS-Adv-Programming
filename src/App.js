29a2673fb9531689c9dec127ae5da97dd363dd75
Maxton Green <GreenMC@cougars.sf.edu>
2024-05-03 17:35:19 -0400

    Added Checkout Page

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ const Navbar = () => {
         <li><Link to="/">Home</Link></li>
         <li><Link to="/shop">Shop</Link></li>
         <li><Link to="/cart">Cart</Link></li>
+        <li><Link to="/checkout">Checkout</Link></li>
       </ul>
     </nav>
   );
